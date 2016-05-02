a32f536e90e9a1aaf7ed3ffcb640cee95d9d94c4
Raul Romo <difuzzor@gmail.com>
2016-05-01 22:59:39 -0500

    juegoMemoria

---
+++ juegoMemoria.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var rango = 0...100
+
+for i in rango {
+    
+    if i % 5 == 0 {
+        print("# \(i)" + " Bingo!!!")
+    }
+    
+    if i % 2 == 0 {
+        print("# \(i)" + " par!!!")
+    }
+    
+    if i % 2 == 1 {
+        print("# \(i)" + " impar!!!")
+    }
+    
+    if i >= 30 && i <= 40  {
+        print("# \(i)" + " Viva Swift!!!")
+    }
+}
+
